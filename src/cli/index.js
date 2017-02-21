5fca501407c21665a9ab3dcb436ab9f74917a7c1
Louis Vichy <lgvichy@gmail.com>
2017-02-20 23:27:45 -0800

    CLI and API looks good. Now to shape the API for good

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,3 +1,32 @@
-console.log("Hello");
+import commander from 'commander';
+import chalk from 'chalk';
+import WTF from '../api';
+const log = console.log;
 
-console.log(process.argv[2]);
+function processFile(filename) {
+
+	const resp = WTF.is(filename);
+
+	log(`
+  ${chalk.green(filename)}
+
+  Usage:
+
+   ${resp.usage}
+
+  Description:
+
+   ${resp.description}
+    `);
+}
+
+commander
+	.version('0.0.1')
+	.command('<filename>', 'Tell you what that file was supposed to do')
+	.action(processFile);
+
+commander
+	.command('is <filename>')
+	.action(processFile);
+
+commander.parse(process.argv);
